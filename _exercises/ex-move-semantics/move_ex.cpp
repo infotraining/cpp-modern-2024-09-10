891575c3a7acb65849b7d94f5927742ba3bd8cae
Krystian Piękoś <krystian.piekos@infotraining.pl>
2024-09-12 10:13:37 +0200

    Tyoe traits

--- _exercises/ex-move-semantics/move_ex.cpp
+++ _exercises/ex-move-semantics/move_ex.cpp
@@ -4,17 +4,26 @@
 #include <iostream>
 #include <optional>
 #include <string_view>
+#include <type_traits>
+
+using namespace std::literals;
 
 // Modernize the following code using move semantics and/or perfect forwarding
 class Player
 {
 public:
-    Player(int id, const std::string& name, const std::vector<int>& scores)
-        : id_(id)
-        , name_(name)
-        , scores_(scores)
-    {
-    }
+    // Player(int id, const std::string& name, const std::vector<int>& scores)
+    //     : id_(id)
+    //     , name_(name)
+    //     , scores_(scores)
+    // {
+    // }
+
+    Player(int id, std::string name, std::vector<int> scores)
+        : id_{id}
+        , name_{std::move(name)}
+        , scores_{std::move(scores)}
+    { }
 
     const int id() const { return id_; }
     const std::string& name() const { return name_; }
@@ -36,16 +45,15 @@ TEST_CASE("Modernizing Player class")
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-
 // Why this function is buggy?
 
 template <typename T>
 auto make_vector(T&& obj)
-{
+{    
     using TValue = std::remove_cvref_t<T>;
     std::vector<TValue> vec;
 
-    vec.push_back(std::move(obj));
+    vec.push_back(std::forward<T>(obj));
 
     //...
 
@@ -54,12 +62,24 @@ auto make_vector(T&& obj)
 
 TEST_CASE("Bug#1")
 {
-    std::string text = "Text.......................";
+    SECTION("lvalue")
+    {
+        std::string text = "Text.......................";
 
-    auto vec = make_vector(text);
-    REQUIRE(vec.front() == "Text.......................");
+        auto vec = make_vector(text);
+        REQUIRE(vec.front() == "Text.......................");
 
-    // REQUIRE(text == "Text.......................");
+        REQUIRE(text == "Text.......................");
+    }
+
+    SECTION("rvalue")
+    {        
+        auto vec = make_vector(std::string("Text......................."));
+        REQUIRE(vec.front() == "Text.......................");
+
+        std::string text = "Text.......................";
+        auto vec2 = make_vector(std::move(text));
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -81,16 +101,27 @@ struct Value
     }
 };
 
-
-
 template <typename T>
 struct Data
 {
     T data;
-    
-    void push(T&& value)
+
+    static_assert(!std::is_reference_v<T>, "T cannot be a reference type");
+
+    // void push(const T& value)
+    // {
+    //     data = value;
+    // }
+
+    // void push(T&& value)
+    // {
+    //     data = std::move(value);
+    // }
+
+    template <typename U>
+    void push(U&& value)
     {
-        data = std::forward<T>(value);
+        data = std::forward<U>(value);
     }
 };
 
@@ -99,7 +130,7 @@ TEST_CASE("Bug#2")
     SECTION("what will happen? - 1")
     {
         Data<Value> data;
-        
+
         Value value;
         data.push(std::move(value));
     }
@@ -109,6 +140,16 @@ TEST_CASE("Bug#2")
         Data<Value> data;
         Value value;
 
-        // data.push(value); // uncomment the line - why this does not compile?
+        data.push(value); // uncomment the line - why this does not compile?
+    }
+
+    SECTION("Data<string>")
+    {
+        Data<std::string> data;
+
+        data.push("text"s);
+        data.push("text");
+
+        //Data<std::string&> data2;
     }
 }
--- type-traits/type_traits.cpp
+++ type-traits/type_traits.cpp
@@ -5,7 +5,61 @@
 
 using namespace std::literals;
 
+bool is_large(int x)
+{
+    return x > 8;
+}
+
+template <typename T>
+struct IsLarge
+{
+    constexpr static bool value = sizeof(T) > 8;
+};
+
+// template variable
+template <typename T>
+constexpr bool IsLarge_v = IsLarge<T>::value;
+
 TEST_CASE("type traits")
 {
-    CHECK(true);
+    static_assert(IsLarge<std::string>::value);
+    static_assert(!IsLarge<bool>::value);
+    static_assert(IsLarge_v<std::vector<int>>);
+}
+
+template <typename T>
+struct Identity
+{
+    using type = T;
+};
+
+template <typename T>
+using Identity_t = typename Identity<T>::type;
+
+///////////////////////////////////////////////////
+
+template <typename T1, typename T2>
+struct IsSame
+{
+    constexpr static bool value = false;
+};
+
+template <typename T>
+struct IsSame<T, T>
+{
+    constexpr static bool value = true;
+};
+
+template<typename T1, typename T2>
+constexpr bool IsSame_v = IsSame<T1, T2>::value;
+
+///////////////////////////////////////////////////
+
+TEST_CASE("Identity")
+{
+    Identity<int>::type x;
+
+    static_assert(IsSame<Identity_t<int>, int>::value);
+    static_assert(IsSame_v<int, int>);
+    static_assert(!IsSame_v<int, const int>);
 }
