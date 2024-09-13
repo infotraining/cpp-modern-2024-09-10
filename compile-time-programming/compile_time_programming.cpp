49bb9b6d2dfead1cd335f4e7aba980499aa232ee
Krystian Piękoś <krystian.piekos@infotraining.pl>
2024-09-13 16:04:15 +0200

    THE END

--- compile-time-programming/compile_time_programming.cpp
+++ compile-time-programming/compile_time_programming.cpp
@@ -3,12 +3,49 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <array>
+#include <algorithm>
+#include <ranges>
 
 using namespace std::literals;
 
+template <size_t N>
+constexpr auto create_lookup_table()
+{
+    std::array<uint32_t, N> squares{};
+    std::vector<int> vec_squares;
+    vec_squares.reserve(N);
+
+    auto squares_view = std::views::iota(1) 
+        | std::views::transform([](int a) { return a * a; }) 
+        | std::views::take(N);
+
+    std::ranges::copy(squares_view, std::back_inserter(vec_squares));
+    std::ranges::copy(vec_squares, squares.begin());
+
+    //vec_squares[1024] = 665;
+
+    // int* ptr = nullptr;
+    // *ptr = 665;
+
+    return squares;
+}
+
+TEST_CASE("lookup table")
+{
+    constexpr auto squares_lookup_table = create_lookup_table<100>();
+}
+
+consteval int cube(int x)
+{
+    return x * x * x;
+}
+
 TEST_CASE("consteval")
 {
-    REQUIRE(true);
+    int c = cube(11);
+
+    std::vector cubes = { cube(9), cube(11), cube(55) };
 }
 
 TEST_CASE("constexpr extensions")
